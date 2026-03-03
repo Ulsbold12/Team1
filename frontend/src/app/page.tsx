3cd30bafc14e59e9ffce6fe241af8338d0db7e14
ssol20071127-eng <ssol20071127@gmail.com>
2026-03-03 14:53:55 +0800

    landing page end

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,19 @@
-<<<<<<< HEAD
-"use client"
+"use client";
 
-import Hero from "@/app/_components/Hero"
-import Trusted from "./_components/Trusted"
-import Footer from "./_components/Footer"
-import Modul from "./_components/Modul"
-import Cta from "./_components/Cta"
+import Hero from "@/app/_components/Hero";
+import Trusted from "./_components/Trusted";
+import Footer from "./_components/Footer";
+import Modul from "./_components/Modul";
+import Cta from "./_components/Cta";
 
-export default function Home () {
-    return(
-        <div className="">
-            <Hero/>
-            <Trusted/>
-            <Modul/>
-            <Cta/>
-            <Footer/>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div className="">
+      <Hero />
+      <Trusted />
+      <Modul />
+      <Cta />
+      <Footer />
+    </div>
+  );
 }
-
-
