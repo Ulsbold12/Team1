ec12bfcea1900b9320efa443eecd278c5078e71c
Munkhuu0000 <munhbat4486@gmail.com>
2026-03-04 10:46:53 +0800

    finance start-1

---
+++ frontend/src/app/(client)/finance/page.tsx
@@ -0,0 +1,16 @@
+import { AISection } from "./_components/AISection";
+import { Dashboard } from "./_components/Dashboard";
+import { GraphicSection } from "./_components/GraphicSection";
+import { HeadSection } from "./_components/HeadSection";
+
+export default function Finance() {
+  return (
+    <div>
+      <HeadSection />
+      <Dashboard />
+      <AISection />
+      <GraphicSection />
+
+    </div>
+  );
+}
