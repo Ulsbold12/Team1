0fbac0476b111508f33b6f147af39a5a4865559e
Ulsbold12 <ulsboldulsbold921@gmail.com>
2026-03-10 12:13:05 +0800

    onboarding bug fixed

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider signInFallbackRedirectUrl="/dashboard" signUpFallbackRedirectUrl="/onboarding">
       <html lang="en" suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased `}
